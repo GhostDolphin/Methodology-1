89abe17aa4090836d12214f0c14cd4e69c560764
GhostDolphin <d.ghost2ac@gmail.com>
2023-04-12 12:55:12 +0300

    Removed unnecessary dependancy

--- code/core.js
+++ code/core.js
@@ -2,7 +2,6 @@
 
 import { getInteractive } from './interactive.js';
 import { getUninteractive } from './uninteractive.js';
-import { parseEquation, getDisc, calculate } from './calc.js';
 
 if (process.argv.length < 3)
   getInteractive();
